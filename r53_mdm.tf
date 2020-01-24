17f71e56c37ac7d8eab1df071a009a6fa44f2923
Sebastian J <zeeman@zeeman.de>
2020-01-24 19:29:56 +0100

    Fix MDM deactivation

--- r53_mdm.tf
+++ r53_mdm.tf
@@ -1,4 +1,6 @@
 resource "aws_route53_record" "enterpriseregistration" {
+  count = var.enable_mdm == true ? 1 : 0
+  
   zone_id = var.zone.id
   name    = "enterpriseregistration.${var.zone.name}"
   type    = "CNAME"
@@ -9,6 +11,8 @@ resource "aws_route53_record" "enterpriseregistration" {
 
 
 resource "aws_route53_record" "enterpriseenrollment" {
+  count = var.enable_mdm == true ? 1 : 0
+
   zone_id = var.zone.id
   name    = "enterpriseenrollment.${var.zone.name}"
   type    = "CNAME"
